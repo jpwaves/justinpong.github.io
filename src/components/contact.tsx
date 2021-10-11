c96059c0b21b30fead68b3619c562cb3353fb185
Justin Pong <jp.waves51@gmail.com>
2021-10-11 03:41:04 -0400

    Add skeleton class component template

---
+++ src/components/contact.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './contact.css';
+
+class Contact extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
+
+export default Contact;
---
+++ src/components/welcome.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './welcome.css';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <>
+
+            </>
+        );
+    }
+}
+
+export default Welcome;
